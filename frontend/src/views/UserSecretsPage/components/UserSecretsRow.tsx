bbb02e6f9b48258f98c9a87733edd14ccabd2a51
Preston Coldwell <pscoldwell@gmail.com>
2024-11-05 08:43:46 -0500

    initial user secrets table

---
+++ frontend/src/views/UserSecretsPage/components/UserSecretsRow.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { faEye, faEyeSlash, faPencilAlt,faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { format } from "date-fns";
+
+import { IconButton, Td, Tr } from "@app/components/v2";
+import { TUserSecret } from "@app/hooks/api/userSecrets";
+import { UsePopUpState } from "@app/hooks/usePopUp";
+
+export const UserSecretsRow = ({
+  row,
+  handlePopUpOpen
+}: {
+  row: TUserSecret;
+  handlePopUpOpen: (
+    popUpName: keyof UsePopUpState<["deleteUserSecretConfirmation", "updateUserSecret"]>,
+    {
+      name,
+      id
+    }: {
+      name: string;
+      id: string;
+    }
+  ) => void;
+}) => {
+  const [isDataVisible, setIsDataVisible] = useState(false);
+
+  const toggleDataVisibility = () => {
+    setIsDataVisible(!isDataVisible);
+  };
+
+  return (
+    <Tr key={row.id}>
+      <Td>{row.name ? `${row.name}` : "-"}</Td>
+      <Td>{row.type ? `${row.type}` : "-"}</Td>
+      <Td>
+        {isDataVisible ? (
+          <div>
+            {Object.entries(row.encryptedData).map(([key, value]) => (
+              <div key={key}>
+                <strong>{key}:</strong> {value}
+              </div>
+            ))}
+          </div>
+        ) : (
+          "******"
+        )}
+      </Td>
+      <Td>{`${format(new Date(row.createdAt), "yyyy-MM-dd - HH:mm a")}`}</Td>
+      <Td>{`${format(new Date(row.updatedAt), "yyyy-MM-dd - HH:mm a")}`}</Td>
+      <Td>
+        <div style={{ display: "flex", gap: "8px" }}>
+          <IconButton
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleDataVisibility();
+            }}
+            variant="plain"
+            ariaLabel="toggle visibility"
+          >
+            <FontAwesomeIcon icon={isDataVisible ? faEyeSlash : faEye} />
+          </IconButton>
+          <IconButton
+            onClick={(e) => {
+              e.stopPropagation();
+              handlePopUpOpen("updateUserSecret", {
+                name: "update",
+                id: row.id
+              });
+            }}
+            variant="plain"
+            ariaLabel="update"
+          >
+            <FontAwesomeIcon icon={faPencilAlt} />
+          </IconButton>
+          <IconButton
+            onClick={(e) => {
+              e.stopPropagation();
+              handlePopUpOpen("deleteUserSecretConfirmation", {
+                name: "delete",
+                id: row.id
+              });
+            }}
+            variant="plain"
+            ariaLabel="delete"
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </IconButton>
+        </div>
+      </Td>
+    </Tr>
+  );
+};
